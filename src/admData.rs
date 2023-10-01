b0857e8707ec913dd1a007e8d171348750b78e1f
chrisateen <c.awofeso@googlemail.com>
2023-10-01 18:56:17 +0100

    Remove only storing p+1 vias

--- src/admData.rs
+++ src/admData.rs
@@ -57,10 +57,6 @@ impl AdmData {
             }
             let entry = self.layer_2.entry(*v).or_default();
             entry.insert(*u);
-            // Only add u as a via if v has less than p + 1 vias
-            //if entry.len() < p + 1 {
-                //entry.insert(*u);
-            //}
         }
         self.update_estimate(p);
     }
