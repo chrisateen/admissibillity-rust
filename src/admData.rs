5698542fbd3a692168a6e0baef63a782552febd0
chrisateen <chrisateen@github.com>
2024-07-12 13:16:28 +0100

    Move most of the logic into admGraph

--- src/admData.rs
+++ src/admData.rs
@@ -1,112 +1,40 @@
-use std::rc::Rc;
-use graphbench::editgraph::EditGraph;
-use graphbench::graph::{Graph, Vertex, VertexMap, VertexSet};
-use graphbench::iterators::EdgeIterable;
+use graphbench::graph::{Vertex, VertexMap, VertexSet};
 
 pub struct AdmData {
-    id: Vertex,
-    pub estimate: usize,
+    pub id: Vertex,
     pub n1_in_l: VertexSet,
-    pub in_candidates: bool,
-    pub m: VertexMap<Rc<AdmData>>, //key vertex v in M and in L, value neighbour of v in M and in R
+    pub n1_in_r: VertexSet,
+    pub deleted_m: bool,
+    pub m: VertexMap<Vertex>, //key vertex v in M and in L, value neighbour of v in M and in R
     pub vias: VertexSet,
-    pub set_vias: bool
 }
 
 impl AdmData {
     pub fn new(v: Vertex, v_neighbours:VertexSet) -> Self{
         AdmData {
             id: v,
-            estimate: v_neighbours.len(),
             n1_in_l: v_neighbours,
-            in_candidates: false,
+            n1_in_r: VertexSet::default(),
+            deleted_m: false,
             m:VertexMap::default(),
             vias: VertexSet::default(),
-            set_vias: false
         }
     }
 
     //When a vertex is added to candidates we no longer need M
     pub fn delete_m(&mut self){
         self.m = VertexMap::default();
-        self.in_candidates = true;
+        self.deleted_m = true;
     }
 
-    //When a vertex in N1(v) and in L is moving to R remove from L1 and see if it can be added to M
-    pub fn remove_n1_in_l(&mut self, v:Rc<AdmData>)  {
-        self.n1_in_l.remove(&v.id);
-        self.estimate -= 1;
-
-        for u in &v.n1_in_l {
-            if !self.m.contains_key(&u) && !self.n1_in_l.contains(&u) && (u.clone() != self.id){
-                self.m.insert(*u, Rc::clone(&v));
-                self.estimate +=1;
-                return;
-            }
-        }
+    pub fn is_maximal_matching_size_p(&self, p:usize) -> bool {
+        return self.m.len() + self.n1_in_l.len() < p + 1;
     }
 
-    //When a vertex in L and M is moving to R remove from M
-    // also check to see if we can replace the edge being removed
-    pub fn remove_from_m(&mut self, v:Rc<AdmData>){
-        //remove v from m if v is in m
-        let v_neighbour_in_m = self.m.remove(&v.id);
-
-        match v_neighbour_in_m {
-            None => return,
-            Some(x) => {
-                self.estimate -=1;
-                //check if there is another vertex that can replace v
-                for y in &x.n1_in_l {
-                    if !self.m.contains_key(&y) && !self.n1_in_l.contains(&y) && (y.clone() != self.id) && (y.clone() != x.id){
-                        self.m.insert(*y, Rc::clone(&x));
-                        self.estimate +=1;
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-
-    fn add_vias(&mut self, graph: &EditGraph, p:usize){
-        let mut counter : VertexMap<usize> = VertexMap::default();
-        let neighbours_v = graph.neighbours(&self.id);
-
-        let _ = self.n1_in_l.iter().map(|x| self.vias.insert(*x));
-        let _ = self.m.values().map(|x| self.vias.insert(x.id.clone()));
-
-        //For each L in M count how many neighbours it has in R and M
-        for (u,_) in &self.m {
-            for w in self.m.values(){
-                if w.n1_in_l.contains(&u){
-                    *counter.entry(*u).or_default() += 1;
-                }
-            }
-        }
-
-        for w in neighbours_v{
-            if !(self.m.contains_key(&w) && self.n1_in_l.contains(&w) ){
-                for (u,_) in &self.m {
-                    if graph.adjacent(u,w){
-                        *counter.entry(*u).or_default() += 1;
-
-                        if counter.get(u).unwrap() <= &(p + 1) {
-                            self.vias.insert(*w);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    //TODO
-    fn construct_g_for_augmenting_path(&self) {
-
-    }
-
-    //TODO
-    fn augmenting_path(){
+    //Checks if a vertex v in L is not in M or not in L1
+    //If not that vertex can be added to M
+    pub fn can_add_vertex_in_l_to_m(&self, v:&Vertex) -> bool{
+        return !self.m.contains_key(v) || !self.n1_in_l.contains(v) || v.clone() != self.id
     }
 }
 
--- src/admGraph.rs
+++ src/admGraph.rs
@@ -1,5 +1,5 @@
 use graphbench::editgraph::EditGraph;
-use graphbench::graph::{Graph, VertexMap, VertexSet};
+use graphbench::graph::{Graph, Vertex, VertexMap, VertexSet};
 use crate::admData::AdmData;
 
 pub struct AdmGraph {
@@ -31,8 +31,99 @@ impl AdmGraph {
         }
     }
 
-    pub fn move_vertex_to_r(&mut self, p: usize){
+    //When a vertex v is moving into R remove v as an L1 from all of its neighbours
+    //check if v can be added to M
+    fn update_n1_of_v(&mut self, v:&AdmData){
+        for u in self.graph.neighbours(&v.id){
+            let u_adm_data = self.adm_data.get_mut(u).unwrap();
+            u_adm_data.n1_in_l.remove(&v.id);
+            u_adm_data.n1_in_r.insert(v.id);
+
+            if !u_adm_data.deleted_m{
+                for w in &v.n1_in_l{
+                    if u_adm_data.can_add_vertex_in_l_to_m(w){
+                        u_adm_data.m.insert(*w, v.id);
+                    }
+                }
+            }
+        }
+    }
+
+    //When a vertex v is moving into R for each of v's L2 neighbours replace/remove v from M
+    fn update_l2_of_v(&mut self, v:&AdmData){
+        let l_in_m: Vec<&Vertex>  = v.m.keys().collect();
+
+        for u in l_in_m{
+            let u_adm_data = &mut self.adm_data.get_mut(u).unwrap();
+
+            //remove v from m of u if v is in m of u
+            let v_neighbour_in_m = u_adm_data.m.remove(&v.id);
+
+            match v_neighbour_in_m {
+                None => return,
+                //check to see if we can replace the edge being removed
+                Some(x) => {
+                    let x_adm_data = self.adm_data.get(&x).unwrap();
+                    //check if there is another vertex that can replace v
+                    for y in &x_adm_data.n1_in_l {
+                        if u_adm_data.can_add_vertex_in_l_to_m(y) {
+                            u_adm_data.m.insert(*y, x);
+                            break;
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    fn add_vias(&mut self, p:usize){
+        // let mut counter : VertexMap<usize> = VertexMap::default();
+        // let neighbours_v = graph.neighbours(&self.id);
+        //
+        // let _ = self.n1_in_l.iter().map(|x| self.vias.insert(*x));
+        // let _ = self.m.values().map(|x| self.vias.insert(x.id.clone()));
+        //
+        // //For each L in M count how many neighbours it has in R and M
+        // for (u,_) in &self.m {
+        //     for w in self.m.values(){
+        //         if w.n1_in_l.contains(&u){
+        //             *counter.entry(*u).or_default() += 1;
+        //         }
+        //     }
+        // }
+        //
+        // for w in neighbours_v{
+        //     if !(self.m.contains_key(&w) && self.n1_in_l.contains(&w) ){
+        //         for (u,_) in &self.m {
+        //             if graph.adjacent(u,w){
+        //                 *counter.entry(*u).or_default() += 1;
+        //
+        //                 if counter.get(u).unwrap() <= &(p + 1) {
+        //                     self.vias.insert(*w);
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+    }
+
+    fn move_vertex_to_r(&mut self, p: usize){
         
     }
 
+    //TODO
+    fn construct_g_for_augmenting_path(&self) {
+
+    }
+
+    //TODO DFS
+    fn augmenting_path(){
+    }
+
+}
+
+#[cfg(test)]
+mod test_adm_graph {
+    //TODO
 }
