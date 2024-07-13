e32794a2e990a197fe1f53b29dd7eb2e7c7c7f7b
chrisateen <chrisateen@github.com>
2024-07-13 18:10:21 +0100

    Add tests for admData

--- src/admData.rs
+++ src/admData.rs
@@ -39,16 +39,102 @@ impl AdmData {
     //Checks if a vertex v in L is not in M or not in L1
     //If not that vertex can be added to M
     pub fn can_add_vertex_in_l_to_m(&self, v:&Vertex) -> bool{
-        return !self.m.contains_key(v) || !self.n1_in_l.contains(v) || v.clone() != self.id
+        return !(self.m.contains_key(v) || self.n1_in_l.contains(v) || v.clone() == self.id)
     }
 
     pub fn initialise_vias(&mut self) {
         self.vias.extend(&self.n1_in_r);
-        let _ = self.m.values().map(|x| self.vias.insert(*x));
+        self.vias.extend(self.m.values());
     }
 }
 
 #[cfg(test)]
 mod test_adm_data {
-    //TODO
+    use graphbench::graph::{Vertex, VertexMap, VertexSet};
+    use crate::admData::AdmData;
+
+    #[test]
+    fn delete_m_should_reset_m() {
+        let neighbours = vec![2, 3, 4, 5].iter().cloned().collect();
+        let mut v = AdmData::new(1,neighbours);
+        v.m.insert(6,7);
+        v.m.insert(9,10);
+        v.delete_m();
+
+        assert!(v.deleted_m);
+        assert_eq!(v.m.len(), 0);
+    }
+
+    #[test]
+    fn is_maximal_matching_size_p_should_check_if_len_l_and_m_is_p(){
+        let neighbours = vec![2, 3, 4, 5].iter().cloned().collect();
+        let mut v = AdmData::new(1,neighbours);
+        v.m.insert(6,7);
+        v.m.insert(9,10);
+
+        assert!(!v.is_maximal_matching_size_p(5));
+        assert!(v.is_maximal_matching_size_p(6));
+        assert!(v.is_maximal_matching_size_p(7));
+    }
+
+    #[test]
+    fn move_n1_in_l_to_r_should_remove_vertex_in_l_and_add_to_r(){
+        let neighbours = vec![2, 3, 4, 5].iter().cloned().collect();
+        let mut v = AdmData::new(1,neighbours);
+
+        v.move_n1_in_l_to_r(&2);
+
+        assert_eq!(v.n1_in_l.len(), 3);
+        assert_eq!(v.n1_in_r.len(), 1);
+    }
+
+    #[test]
+    fn can_add_vertex_in_l_to_m_returns_false_if_v_is_in_l(){
+        let neighbours = vec![2, 3, 4, 5].iter().cloned().collect();
+        let mut v = AdmData::new(1,neighbours);
+
+        assert!(!v.can_add_vertex_in_l_to_m(&2));
+    }
+
+    #[test]
+    fn can_add_vertex_in_l_to_m_returns_false_if_v_has_same_id_as_self(){
+        let neighbours = vec![2, 3, 4, 5].iter().cloned().collect();
+        let mut v = AdmData::new(1,neighbours);
+
+        assert!(!v.can_add_vertex_in_l_to_m(&1));
+    }
+
+    #[test]
+    fn can_add_vertex_in_l_to_m_returns_false_if_v_is_in_m() {
+        let neighbours = vec![2, 3, 4, 5].iter().cloned().collect();
+        let mut v = AdmData::new(1,neighbours);
+        v.m.insert(6,7);
+
+        assert!(!v.can_add_vertex_in_l_to_m(&6));
+    }
+
+    #[test]
+    fn can_add_vertex_in_l_to_m_returns_false_if_v_is_not_in_m_or_l() {
+        let neighbours = vec![2, 3, 4, 5].iter().cloned().collect();
+        let mut v = AdmData::new(1,neighbours);
+        v.m.insert(6,7);
+
+        assert!(v.can_add_vertex_in_l_to_m(&8));
+    }
+
+    #[test]
+    fn initialise_vias_should_add_vertices_in_r(){
+        let l1 = vec![2, 3, 4, 5].iter().cloned().collect();
+        let r1 = vec![6,7,8].iter().cloned().collect();
+        let mut v = AdmData::new(1, l1);
+        v.n1_in_r = r1;
+
+        v.m.insert(9,10);
+        v.m.insert(11,12);
+
+        v.initialise_vias();
+
+        assert_eq!(v.vias, vec![6,7,8,10,12].iter().cloned().collect());
+
+    }
 }
